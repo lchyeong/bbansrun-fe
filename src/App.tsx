93991a2292f8a8b2cf77f577fd161bc8e14a37fe
lchyeong <lchyeong@gmail.com>
2024-09-01 17:55:06 +0900

    feat: react router doom 설정, 공통 컨테이너 설정 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-// import AppRouter from './router/AppRouter'
-import { useState } from 'react'
+import "./styles/global.css"; // 글로벌 스타일 불러오기
 
 function App() {
-  
   return (
-    <>
-      
-    </>
-  )
+    <div className="container">
+      <h1>Welcome BBANS RUN</h1>
+      <p>This is the main page of the application.</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,12 +1,13 @@
 const colors = {
-    primary: '#1976d2',
-    secondary: '#dc004e',
-    success: '#4caf50',
-    danger: '#f44336',
-    warning: '#ff9800',
-    info: '#2196f3',
-    light: '#f5f5f5',
-    dark: '#212121',
-  };
-  
-  export default colors;
+  primary: "#1976d2", // 주 색상
+  secondary: "#dc004e", // 보조 색상
+  success: "#4caf50", // 성공 상태 색상
+  danger: "#f44336", // 위험 상태 색상
+  warning: "#ff9800", // 경고 상태 색상
+  info: "#2196f3", // 정보 상태 색상
+  light: "#f5f5f5", // 밝은 색상 (배경 등)
+  dark: "#212121", // 어두운 색상 (텍스트 등)
+  background: "#ffffff", // 기본 배경 색상
+};
+
+export default colors;
