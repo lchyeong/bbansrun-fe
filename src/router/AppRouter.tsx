c0f14974aad782e8dc08dfc52142d55e57d5d1fe
lchyeong <lchyeong@gmail.com>
2024-09-01 14:12:34 +0900

    feat: 샘플 TSX 파일 생성

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,21 @@
+// import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+// import About from '../pages/About';
+// import Home from '../pages/Home';
+// import NotFound from '../pages/NotFound';
+// import React from 'react';
+
+// const AppRouter: React.FC = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         {/* 존재하지 않는 경로 처리 */}
+//         <Route path="*" element={<NotFound />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default AppRouter;
